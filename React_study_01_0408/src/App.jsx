03096ade89a968d895e4c9ee998a0560f5fa697a
EunHyeok <secun77@naver.com>
2025-04-10 11:50:03 +0900

    useEffect 기초

--- React_study_01_0408/src/App.jsx
+++ React_study_01_0408/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // @ts-ignore
 import styles from './App.module.css';
 import MainHeader from './components/SideEffect/MainHeader/MainHeader';
@@ -34,19 +34,33 @@ const App = () => {
     localStorage.removeItem('login-flag');
   };
 
-  // 화면이 렌더링 될 때 localStroage를 확인해서
-  // login-flag라는 데이터가 있다면 로그인 상태를 변경하자!
-  // 로그인 후, 새로고침이나 브라우저 종료 후 재접속 시 로그인 상태 유지를 위해서
-  const storedLoginFlag = localStorage.getItem('login-flag');
-  // login-flag가 있다면 로그인했으니 바로 Home으로 이동하게 true
-  if (storedLoginFlag === '1') {
-    setIsLoginValid(true);
-  }
-  // login-flag가 없다면 로그인하지 않거나
-  // 로그아웃한 것이니 로그인창으로 이동하게 false
-  else {
-    setIsLoginValid(false);
-  }
+  // useEffect는 2개의 매개값을 받음
+  // 콜백함수, 의존성배열(배열 안 요소가 변화하면 useEffect가 실행됨)
+  // 의존성 배열을 비워놓으면 첫 렌더링만 useEffect가 실행됨을 보장받음.
+  useEffect(() => {
+    // 이 useEffect의 콜백함수는 최초 렌더링 시에만 실행되면 되므로
+    // 의존성 배열을 비워놓는게 좋겠다!!
+    console.log('useEffect가 실행됨');
+
+    // 화면이 렌더링 될 때 localStroage를 확인해서
+    // login-flag라는 데이터가 있다면 로그인 상태를 변경하자!
+    // 로그인 후, 새로고침이나 브라우저 종료 후 재접속 시 로그인 상태 유지를 위해서
+
+    const storedLoginFlag = localStorage.getItem('login-flag');
+
+    // login-flag가 있다면 로그인했으니 바로 Home으로 이동하게 true
+    if (storedLoginFlag === '1') {
+      setIsLoginValid(true);
+    }
+    // login-flag가 없다면 로그인하지 않거나
+    // 로그아웃한 것이니 로그인창으로 이동하게 false
+    else {
+      setIsLoginValid(false);
+    }
+  }, []);
+  // 의존성 배열: useEffect가 실행되어야 하는 트리거 변수.
+  // 배열 안에 상태 변수를 지정하면, 해당 변수의 값이 변할 때마다 useEffect가 실행되고,
+  // 만약 빈 배열을 전달하면 최초 렌더링 과정에서 단 한번만 실행.
 
   return (
     <>
