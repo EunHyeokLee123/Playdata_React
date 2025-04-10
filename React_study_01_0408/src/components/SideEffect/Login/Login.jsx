47c2b704df6fc66eb49219183810e2ae0dbaae09
EunHyeok <secun77@naver.com>
2025-04-10 14:48:32 +0900

    useReducer 기초

--- React_study_01_0408/src/components/SideEffect/Login/Login.jsx
+++ React_study_01_0408/src/components/SideEffect/Login/Login.jsx
@@ -1,28 +1,75 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import Card from '../../../UI/Card';
 // @ts-ignore
 import styles from './Login.module.css';
 import Button from '../../../UI/Button';
 
-const Login = ({ onLogin }) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPW, setEnteredPW] = useState('');
+const initialLoginState = {
+  enteredEmail: '',
+  enteredPW: '',
+  emailIsValid: null,
+  pwIsValid: null,
+};
 
-  // 이메일 입력의 유효여부를 상태 변수로 관리
-  const [emailIsValid, setEmailIsValid] = useState(); // 초기값 undefined
-  const [pwIsValid, setPWIsValid] = useState();
+// 리듀서 함수 선언
+/*
+   이 컴포넌트에서 사용하는 모든 상태와 상태 변경을 중앙 제어하는 함수.
+   컴포넌트 내부 데이터를 사용하지 않고 상태에만 집중하기 때문에
+   컴포넌트 바깥쪽에 선언하는 것이 일반적입니다.
+   
+   param1 - state: 변경 전의 상태값
+   param2 - action: dispatch함수(상태 변경 등의 행동)가 전달한 상태 변경 객체
+   return: 관리할 상태값들을 반환
+ */
+
+const loginReducer = (state, action) => {
+  switch (action.type) {
+    case 'EMAIL_INPUT':
+      return {
+        ...state,
+        enteredEmail: action.val,
+      };
+    case 'PASSWORD_INPUT':
+      return {
+        ...state,
+        enteredPW: action.val,
+      };
+    case 'EMAIL_VALID':
+      return {
+        ...state,
+        emailIsValid: state.enteredEmail.includes('@'),
+      };
+    case 'PASSWORD_VALID':
+      return {
+        ...state,
+        pwIsValid: state.enteredPW.trim().length > 6,
+      };
+    default:
+      return state;
+  }
+};
+
+const Login = ({ onLogin }) => {
+  // loginReducer 함수를 사용해보자
+  /*
+     param1 - reducer function: 위에서 만든 리듀서 함수
+     param2 - initial state: 초기 상태값
+     return1 - 로그인 관련 상태변수
+     return2 - dispatch함수: 상태를 변경할 수 있는 함수
+  */
+  const [loginState, dispatch] = useReducer(loginReducer, initialLoginState);
+
+  // 디스트럭쳐링으로 상태값 추출
+  const { enteredEmail, enteredPW, emailIsValid, pwIsValid } = loginState;
 
   useEffect(
     () => {
-      console.log('Login에서 useEffect가 실행됨~');
-
       // setTimeout을 통해 유효성 검증을 입력이 끝난 1초 뒤에 실행되게 함.
       // 1초 이내에 새로운 입력값이 들어오면, useEffect가 다시 실행되면서
       // 또다른, setTimeout이 실행됨 -> 너무 많은 중복 타이머
       const timer = setTimeout(() => {
-        validateEmailHandler();
-        validatePWHandler();
-        console.log('setTimeout이 호출됨~');
+        dispatch({ type: 'EMAIL_VALID' });
+        dispatch({ type: 'PASSWORD_VALID' });
       }, 1000);
       // useEffect의 cleanup 함수
       // 컴포넌트가 업데이트 되거나 없어지기 직전에 실행됨.
@@ -32,36 +79,35 @@ const Login = ({ onLogin }) => {
       return () => {
         clearTimeout(timer);
       };
-    }, // 상태 변수가 변경되면, 콜백함수가 실행되게 함.
-
+    },
+    // 상태 변수가 변경되면, 콜백함수가 실행되게 함.
     [enteredEmail, enteredPW],
   );
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log('submit이 동작됨!');
 
     onLogin(enteredEmail, enteredPW);
   };
 
   // 이메일의 입력값이 변경될 때마다 실행한 이벤트 핸들러
   const emailChangeHandler = (e) => {
-    setEnteredEmail(e.target.value);
+    // reducer의 상태 변경은 dispatch 함수를 통해서 처리
+    // dispatch함수의 매개값 객체의 key는 정해진 것이 아닌,
+    // reducer 함수에서 구분하기 위해 붙여주는 이름.
+    // 프로퍼티의 key와 value는 자유롭게 줄 수 있습니다. (정해진 게 아님!)
+    dispatch({
+      type: 'EMAIL_INPUT', // 변경되는 데이터의 타입
+      val: e.target.value, // 변경되는 데이터 값
+    });
   };
 
   // 비밀번호의 입력값이 변경될 때마다 실행할 이벤트 핸들러
   const pwChangeHandler = (e) => {
-    setEnteredPW(e.target.value);
-  };
-
-  const validateEmailHandler = () => {
-    // 이메일에 @ 여부
-    setEmailIsValid(enteredEmail.includes('@'));
-  };
-
-  const validatePWHandler = () => {
-    // 비밀번호 길이 확인
-    setPWIsValid(enteredPW.trim().length > 6);
+    dispatch({
+      type: 'PASSWORD_INPUT',
+      val: e.target.value,
+    });
   };
 
   return (
