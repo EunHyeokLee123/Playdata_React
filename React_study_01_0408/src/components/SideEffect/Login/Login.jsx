ba432d00bef746c10b7dcae0c0ea7de3748ecbaf
EunHyeok <secun77@naver.com>
2025-04-10 12:27:58 +0900

    cleanup 기초

--- React_study_01_0408/src/components/SideEffect/Login/Login.jsx
+++ React_study_01_0408/src/components/SideEffect/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Card from '../../../UI/Card';
 // @ts-ignore
 import styles from './Login.module.css';
@@ -11,6 +11,31 @@ const Login = ({ onLogin }) => {
   // 이메일 입력의 유효여부를 상태 변수로 관리
   const [emailIsValid, setEmailIsValid] = useState(); // 초기값 undefined
   const [pwIsValid, setPWIsValid] = useState();
+
+  useEffect(
+    () => {
+      console.log('Login에서 useEffect가 실행됨~');
+
+      // setTimeout을 통해 유효성 검증을 입력이 끝난 1초 뒤에 실행되게 함.
+      // 1초 이내에 새로운 입력값이 들어오면, useEffect가 다시 실행되면서
+      // 또다른, setTimeout이 실행됨 -> 너무 많은 중복 타이머
+      const timer = setTimeout(() => {
+        validateEmailHandler();
+        validatePWHandler();
+        console.log('setTimeout이 호출됨~');
+      }, 1000);
+      // useEffect의 cleanup 함수
+      // 컴포넌트가 업데이트 되거나 없어지기 직전에 실행됨.
+      // 사용자가 1초 이내에 추가 입력 -> 상태 변경(컨포넌트가 리렌더링)
+      // -> 타이머 취소 (유효성 검증을 취소)
+      // -> 마지막에 발생한 이벤트에 대해서만 타이머가 걸림.
+      return () => {
+        clearTimeout(timer);
+      };
+    }, // 상태 변수가 변경되면, 콜백함수가 실행되게 함.
+
+    [enteredEmail, enteredPW],
+  );
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -51,7 +76,7 @@ const Login = ({ onLogin }) => {
             id='email'
             value={enteredEmail}
             onChange={emailChangeHandler}
-            onBlur={validateEmailHandler} // blur는 focus의 반대
+            // onBlur={validateEmailHandler} // blur는 focus의 반대
           />
         </div>
         <div
@@ -63,7 +88,7 @@ const Login = ({ onLogin }) => {
             id='password'
             value={enteredPW}
             onChange={pwChangeHandler}
-            onBlur={validatePWHandler}
+            // onBlur={validatePWHandler}
           />
         </div>
         <div className={styles.actions}>
