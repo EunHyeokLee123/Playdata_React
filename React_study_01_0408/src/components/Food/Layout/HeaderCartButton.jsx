f2a04bc0ffbf9ec7372b905f75a2b6f2ce12bc5e
EunHyeok <secun77@naver.com>
2025-04-11 11:48:58 +0900

    간단한 애니메이션 처리

--- React_study_01_0408/src/components/Food/Layout/HeaderCartButton.jsx
+++ React_study_01_0408/src/components/Food/Layout/HeaderCartButton.jsx
@@ -2,18 +2,44 @@ import CartIcon from '../Cart/CartIcon';
 import { CartContext } from '../Store/CartContext';
 // @ts-ignore
 import styles from './HeaderCartButton.module.scss';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 const HeaderCartButton = ({ onShow }) => {
-  const { button, icon, badge } = styles;
+  // bump 애니메이션을 제어하는 상태 변수
+  const [isBump, setIsBump] = useState(false);
+
+  const { button, icon, badge, bump } = styles;
 
   const { items } = useContext(CartContext);
 
   // 배열 누산 합계 함수: reduce
   const numberOfCart = items.reduce((accu, item) => (accu += item.amount), 0);
 
+  useEffect(
+    () => {
+      setIsBump(true);
+
+      // 다음 담기 애니메이션을 보여주기 위해서는
+      // bump라는 클래스 이름을 제거해야 함.
+      // 단, 애니메이션 동작 시간이 0.3초이므로,
+      // 0.3초 이후(애니메이션 동작 완료 후)에 bump 클래스를 삭제하자.
+      const timer = setTimeout(() => {
+        setIsBump(false);
+      }, 300);
+
+      return () => {
+        // cleanup 함수를 이용하여 0.3초 이내에 새로운 렌더링이 발생하면
+        // 기존의 timeout 함수를 취소한다.
+        // 애니메이션이 버벅이는 것을 방지하기 위해서
+        clearTimeout(timer);
+      };
+    },
+    // items 카트의 상태가 변화할 때마다 useEffect를 실행하겠다!
+    [items],
+  );
+
   return (
-    <button className={button} onClick={onShow}>
+    <button className={`${button} ${isBump ? bump : ''}`} onClick={onShow}>
       <span className={icon}>
         <CartIcon />
       </span>
